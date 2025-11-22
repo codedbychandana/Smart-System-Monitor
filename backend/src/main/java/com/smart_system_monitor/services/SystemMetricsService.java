be9e41ee9ec92ff92c4a97408513cf00f86b07f3
Chandana Ramesh <chandanaramesh516@gmail.com>
2025-11-22 13:05:24 +0530

    feat: create function to return metrics

--- backend/src/main/java/com/smart_system_monitor/services/SystemMetricsService.java
+++ backend/src/main/java/com/smart_system_monitor/services/SystemMetricsService.java
@@ -1,8 +1,13 @@
 package com.smart_system_monitor.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor;
+import oshi.hardware.GlobalMemory;
 import oshi.hardware.HardwareAbstractionLayer;
 
 @Service
@@ -15,5 +20,22 @@ public class SystemMetricsService {
         HAL = sysInfo.getHardware();
     }
 
+    /** 
+     * returns system metrics as a list including cpu load and percentage of memory in use
+    */
+    public List<Double> getMetrics(){
+        // the result
+        List<Double> metrics  = new ArrayList<>();
 
+        CentralProcessor processor = HAL.getProcessor();
+        GlobalMemory memory = HAL.getMemory();
+
+        // calculates cpu load and ram used as a percentage
+        double cpuLoad = processor.getSystemCpuLoad(1000) * 100;
+        double ramUsedPercentage = ((memory.getTotal() - memory.getAvailable()) / memory.getTotal()) * 100;
+        
+        metrics.add(cpuLoad);
+        metrics.add(ramUsedPercentage);
+        return metrics;
+    }
 }
