a47f2212d5bd18fe6f60b62a57919e1f4eaf20b1
Chandana Ramesh <chandanaramesh516@gmail.com>
2025-11-22 13:54:18 +0530

    feat: create overload reduction service

---
+++ backend/src/main/java/com/smart_system_monitor/services/OverloadReductionService.java
@@ -0,0 +1,35 @@
+package com.smart_system_monitor.services;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.springframework.stereotype.Service;
+
+import oshi.SystemInfo;
+import oshi.software.os.OperatingSystem;
+
+@Service
+public class OverloadReductionService {
+    private SystemMetricsService sysMetricsService;
+    private final OperatingSystem OS;
+    // keeps track of cpu load readings
+    private final Queue<Double> cpuLoadHistory;
+
+    public OverloadReductionService(SystemMetricsService sysMetricsService){
+        this.sysMetricsService = sysMetricsService;
+        OS = new SystemInfo().getOperatingSystem();
+        cpuLoadHistory = new LinkedList<>();
+    }
+
+    /**
+     * add cpu load reading to queue
+    */
+    private void addCpuLoad(double cpuLoad){
+        final int MAX_SIZE = 5;
+
+        if (cpuLoadHistory.size() == MAX_SIZE){
+            cpuLoadHistory.poll();
+        }
+        cpuLoadHistory.offer(cpuLoad);
+    }
+}
