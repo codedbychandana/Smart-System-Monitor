1d83874777200d04004db7e69b5b64ae68e15466
Chandana Ramesh <chandanaramesh516@gmail.com>
2025-12-07 13:34:21 +0530

    fix: change threshold method to return threshold based on SCORES not on cpu loads themselves
    method previously computed a threshold based on the cpu loads instead of scores using the ml model. this was erroneous since the return threshold is used to compare with model score

--- backend/src/main/java/com/smart_system_monitor/helpers/IsolationForestManager.java
+++ backend/src/main/java/com/smart_system_monitor/helpers/IsolationForestManager.java
@@ -3,38 +3,76 @@ package com.smart_system_monitor.helpers;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.smart_system_monitor.config.IsolationForestConfig;
+import com.smart_system_monitor.services.MonitoringStatsService;
 
 import smile.anomaly.IsolationForest;
 
 @Component
 public class IsolationForestManager{
-    @Autowired
+    
     private IsolationForest model;
 
-    @Autowired
     private IsolationForestConfig modelConfig;
 
+    private final MonitoringStatsService stats;
+
+    private IsolationForestManager(IsolationForest model, IsolationForestConfig modelConfig, MonitoringStatsService stats){
+        this.model = model;
+        this.modelConfig = modelConfig;
+        this.stats = stats;
+    }
+
     /**
-     * get threshold based on cpu readings
+     * returns model threshold for scores 
     */
-    private double getThreshold(double[] data, double percentile){
-        int n = data.length;
+    private double getScoreThreshold(List<Double> data, double percentile){
+        // compute score for each data point
+        List<Double> scores = data.stream()
+        .map(load -> model.score(new double[]{load}))
+        .collect(Collectors.toList());
+
+        scores.sort((a, b) -> Double.compare(a, b));
+        int n = scores.size();
+
+        // compute score threshold
         int index = (int) Math.ceil(percentile/100 * n) - 1;
-        return data[index];
+        return scores.get(index);
     }
 
     /** 
      * return true if anomaly detected 
     */
-    public boolean isAnomaly(double[] sample, double[] data){
-        double percentile = 85;
+    public boolean isAnomaly(double[] sample, List<Double> data, double threshold){
+        double percentile = 95;
+
+        // check if isolation forest model detects anomaly
+        boolean anomalyDetected = model.score(sample) * 100 > getScoreThreshold(data, percentile);
         
-        return model.score(sample) * 100 > percentile;
+        // check if true anomaly based on user threshold
+        boolean trueAnomaly = sample[0] > threshold;
+        
+        // record anomaly detected
+        if (anomalyDetected){
+            stats.recordAnomaly();
+        }
+        
+        // record true anomaly detected
+        if (trueAnomaly){
+            stats.recordTrueAnomaly();
+        }
+        
+        System.out.println("sample: " + sample[0] + "model score: " + model.score(sample));
+        System.out.println("true: " + trueAnomaly + " detected: " + anomalyDetected);
+        
+        // anomaly detected only if both results are true
+        return trueAnomaly && anomalyDetected;
     }
 
     /**
