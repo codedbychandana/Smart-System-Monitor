1d3f583b0368a456e2ff302f6d1a9bdbcc765b97
Chandana Ramesh <chandanaramesh516@gmail.com>
2025-12-07 13:36:48 +0530

    feat: create separate services to only detect overload and to terminate / take action in case of overload

--- backend/src/main/java/com/smart_system_monitor/services/OverloadReductionService.java
+++ backend/src/main/java/com/smart_system_monitor/services/OverloadReductionService.java
@@ -2,9 +2,11 @@ package com.smart_system_monitor.services;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -19,69 +21,109 @@ import oshi.software.os.OSProcess;
 public class OverloadReductionService {
     private final SystemMetricsService sysMetrics;
 
+    private final MonitoringStatsService stats;
+
     private final IsolationForestManager isolationForestManager;
 
     // keeps track of cpu load readings
-    private final Queue<Double> cpuLoadHistory;
+    private final Deque<Double> cpuLoadHistory;
+
+    // max history dequeue size
+    private final int HISTORY_MAX_SIZE;
+
+    // user specified threshold
+    private double threshold = 90;
 
     @Autowired
-    public OverloadReductionService(SystemMetricsService sysMetrics, IsolationForestManager isolationForestManager){
+    public OverloadReductionService(SystemMetricsService sysMetrics, IsolationForestManager isolationForestManager, MonitoringStatsService stats){
+        this.HISTORY_MAX_SIZE = 15;
+
         this.sysMetrics = sysMetrics;
+        this.stats = stats;
         this.isolationForestManager = isolationForestManager;
-        cpuLoadHistory = new LinkedList<>();
+        cpuLoadHistory = new LinkedList<>(
+            sysMetrics.getProcesses(HISTORY_MAX_SIZE)
+            .stream()
+            .map(p -> p.getProcessCpuLoadCumulative())
+            .toList()
+        );
     }
 
     /**
-     * reduce overload and monitor every 2 seconds
+     * monitors cpu overload and terminates detected overloaded process
     */
-    public ProcessLogItem monitorCpuLoad() throws IOException{
-        double curCpuLoad = sysMetrics.getMetrics().get(0);
-        // add to cpu history
-        addCpuLoad(curCpuLoad);
+    public ProcessLogItem reduceCpuLoad(double threshold) throws IOException{
+        if (monitorCpuLoad(threshold)){
 
-        // reduce overload
-        if (isOverloaded() || predictOverloading()){
-            
+            // terminate top process
             int pid = killTopProcess();
+
+            // record overload prevented
+            stats.recordOverloadPrevented();
+
             return new ProcessLogItem(new Date(), pid, "Process terminated");
         }
+        // no overload detected
         return null;
     }
 
     /**
-     * add cpu load reading to queue
+     * returns true if anomalous cpu overload present
+    */
+    public boolean monitorCpuLoad(double threshold){
+        // get current cpu load
+        double curCpuLoad = sysMetrics.getMetrics().get(0);
+        
+        // update user provided threshold
+        setThreshold(threshold);
+
+        // add to cpu history
+        addCpuLoad(curCpuLoad);
+
+        return isOverloaded() || predictOverloading();
+    }
+
+    /**
+     * add cpu load reading to history
     */
     private void addCpuLoad(double cpuLoad){
-        final int MAX_SIZE = 5;
-
-        if (cpuLoadHistory.size() == MAX_SIZE){
+        if (cpuLoadHistory.size() == HISTORY_MAX_SIZE){
             cpuLoadHistory.poll();
         }
         cpuLoadHistory.offer(cpuLoad);
     }
 
-    /** returns true if cpu is overloaded 
+    /** 
+     * returns true if cpu is overloaded without ml (only rule based) 
     */
     private boolean isOverloaded(){
-        return cpuLoadHistory.stream()
-        .allMatch(load -> load > 80);
+        final double PERCENT = 0.7; 
+        long countThreshold = Math.round(cpuLoadHistory.size() * PERCENT);
+
+        // count how many records of cpu loads are too high
+        long highLoadCount = cpuLoadHistory.stream()
+        .filter(load -> load > threshold)
+        .count();
+        
+        return highLoadCount >= countThreshold;
     }
 
-    /** uses isolation forest to predict overloading possibility 
+    /** 
+     * uses isolation forest to predict overloading possibility 
     */
     private boolean predictOverloading(){
         if (cpuLoadHistory.isEmpty()){
             return false;
         }
-        double[] cpuLoad = new double[]{cpuLoadHistory.peek()};
+        double[] cpuLoad = new double[]{cpuLoadHistory.peekLast()};
 
-        return isolationForestManager.isAnomaly(cpuLoad, cpuLoadHistory.stream()
-            .mapToDouble(Double::valueOf)
-            .toArray()
-        );
+        // use isolation forest model to see if anomaly detected based on user
+        // provided threshold for cpu load
+        return isolationForestManager.isAnomaly(cpuLoad, cpuLoadHistory);
     }
 
-    /** kills process consuming the most cpu load
+    /** 
+     * kills process consuming the most cpu load
     */
     private int killTopProcess() throws IOException{
         // get top 5 running processes in order of cpu usage
@@ -96,5 +138,12 @@ public class OverloadReductionService {
         Runtime.getRuntime().exec("taskkill /PID " + pid + " /F");
         System.out.println("Process " + pid + " has been terminated.");
         return pid;
+    }
+
+    /** 
+     * sets user provided threshold
+    */
+    private void setThreshold(double threshold){
+        this.threshold = Math.abs(threshold);
     }
 }
